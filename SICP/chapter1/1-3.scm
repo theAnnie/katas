baff4711f171dc1071102d73a3bd7520d0eb924c
theAnnie <cypelll95@gmail.com>
2020-10-04 20:17:55 +0200

    Add SICP

---
+++ SICP/chapter1/1-3.scm
@@ -0,0 +1,7 @@
+(define (f x y z)
+ (cond ((and (> x z) (> y z)) (+ (square x) (square y)))
+       ((and (> x y) (> z y)) (+ (square x) (square z)))
+       (else (+ (square y) (square z)))))
+	   
+(define (square x)
+  (* x x))
