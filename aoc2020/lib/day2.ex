4c652e34ac770c073a2ade4454d5056da52bf785
theAnnie <cypelll95@gmail.com>
2020-12-02 10:54:49 +0100

    Make it more pretty - day 2

--- aoc2020/lib/day2.ex
+++ aoc2020/lib/day2.ex
@@ -2,23 +2,29 @@ defmodule Day2 do
   def how_many_pass_are_valid(input) do
     input
     |> Enum.map(fn i ->
-      [range, letter, password] = i |> String.split(" ")
-
-      [{x, _}, {y, _}] =
-        range
-        |> String.split("-")
-        |> Enum.map(&Integer.parse/1)
-
+      [range, letter, password] = String.split(i, " ")
+      {x, y} = parse_range(range)
       letter = String.replace(letter, ":", "")
-
-      occ =
-        password
-        |> String.graphemes()
-        |> Enum.frequencies()
-        |> Map.get(letter, -1)
+      occ = occurrences(password, letter)
 
       occ in x..y
     end)
     |> Enum.count(fn x -> x == true end)
+  end
+
+  defp parse_range(range) do
+    [{x, _}, {y, _}] =
+      range
+      |> String.split("-")
+      |> Enum.map(&Integer.parse/1)
+
+    {x, y}
+  end
+
+  defp occurrences(password, letter) do
+    password
+    |> String.graphemes()
+    |> Enum.frequencies()
+    |> Map.get(letter, 0)
   end
 end
