d35d4f6aa7d4d898ad44d502b1a6d4a8ca59c537
theAnnie <cypelll95@gmail.com>
2020-12-04 00:31:57 +0100

    Add part 2 of second day

--- aoc2020/lib/day2.ex
+++ aoc2020/lib/day2.ex
@@ -27,4 +27,21 @@ defmodule Day2 do
     |> Enum.frequencies()
     |> Map.get(letter, 0)
   end
+
+  # part 2
+  def how_many_pass_are_valid2(input) do
+    input
+    |> Enum.map(fn i ->
+      [range, letter, password] = String.split(i, " ")
+      {x, y} = parse_range(range)
+      letter = String.replace(letter, ":", "")
+      password = String.graphemes(password)
+      x = x - 1
+      y = y - 1
+      a = Enum.at(password,  x) == letter && Enum.at(password,  y) != letter
+      b = Enum.at(password,  x) != letter && Enum.at(password,  y) == letter
+      a || b
+    end)
+    |> Enum.count(fn x -> x == true end)
+  end
 end
