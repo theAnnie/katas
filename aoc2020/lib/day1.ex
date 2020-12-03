d3d091ead0e7c4d6ef807bb53222919bc13947a2
theAnnie <cypelll95@gmail.com>
2020-12-04 00:16:42 +0100

    Add part 2 of first day

--- aoc2020/lib/day1.ex
+++ aoc2020/lib/day1.ex
@@ -7,4 +7,15 @@ defmodule Day1 do
     |> Stream.filter(fn x -> x != nil end)
     |> Enum.uniq()
   end
+
+  # part2
+  def calc2(input) do
+    for x <- input,
+        y <- input,
+        z <- input do
+      if x + y + z == 2020, do: x * y * z
+    end
+    |> Stream.filter(fn x -> x != nil end)
+    |> Enum.uniq()
+  end
 end
