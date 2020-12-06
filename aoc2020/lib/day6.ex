801a7def232e5659c3f72a598cfc38dc247cc856
theAnnie <cypelll95@gmail.com>
2020-12-06 16:06:06 +0100

    Add day6 - part 2

--- aoc2020/lib/day6.ex
+++ aoc2020/lib/day6.ex
@@ -11,4 +11,25 @@ defmodule Day6 do
         |> Stream.map(&Enum.count/1)
         |> Enum.sum()
     end
+
+    # part 2
+    def calc2(input) do
+        input
+        |> String.split("\n")
+        |> Stream.chunk_by(fn x -> x == "" end)
+        |> Stream.filter(fn x -> x != [""]end)
+        |> Stream.map(fn x -> {Enum.join(x), Enum.count(x)} end)
+        |> Stream.map(fn x -> do_calc(x) end)
+        |> Enum.sum()
+    end
+
+    defp do_calc({str, 1}), do: String.length(str)
+
+    defp do_calc({str, n}) do
+        str
+        |> String.graphemes()
+        |> Enum.frequencies()
+        |> Enum.filter(fn {_k,v} -> v == n end)
+        |> Enum.count()
+    end
 end
