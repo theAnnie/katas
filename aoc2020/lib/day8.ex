ef348fe6cb6b814f0844ce3c57d6f691f997823c
theAnnie <cypelll95@gmail.com>
2020-12-09 00:21:55 +0100

    Add day8 part 2

--- aoc2020/lib/day8.ex
+++ aoc2020/lib/day8.ex
@@ -26,4 +26,47 @@ defmodule Day8 do
             "nop"   <> _x -> {value, index + 1}
         end
     end
+
+    #part 2
+    def acc_value2(input) do
+        input 
+        |> String.split("\n", trim: true)
+        |> do_calc2(0, [], 0)
+    end
+
+    defp do_calc2(input, index, visited_indexes, value) do
+        x = input
+        |> Stream.with_index()
+        |> Stream.map(fn {x,y} -> if String.starts_with?(x, "jmp") || String.starts_with?(x, "nop"), do: y end)
+        |> Enum.filter(fn x -> x != nil end)
+
+        do_calc2(input, index, visited_indexes, value, x, input)
+    end
+
+    defp do_calc2(input, index, visited_indexes, value, indexes_to_try, orginal_input) do
+        if Enum.member?(visited_indexes, index) do
+            [h | t] = indexes_to_try |> IO.inspect()
+            input = orginal_input
+            |> Stream.with_index()
+            |> Enum.map(fn {x,y} -> if y == h, do: replace(x), else: x end)
+            do_calc2(input, 0, [], 0, t, orginal_input)
+        else
+            current_op = Enum.at(input, index)
+            case current_op do
+                nil -> 
+                    value
+                _ ->
+                    visited = [index | visited_indexes]
+                    {value, index} = handle_operation(current_op, value, index)
+                    do_calc2(input, index, visited, value, indexes_to_try, orginal_input)
+            end
+        end
+    end
+
+    defp replace(op) do
+        case op do
+            "jmp" <> x -> "nop" <> x
+            "nop" <> x -> "jmp" <> x
+        end
+    end
 end
