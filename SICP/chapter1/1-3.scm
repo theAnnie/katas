657199d7137a88a13adca14cd60d680548e66d71
theAnnie <cypelll95@gmail.com>
2021-01-10 18:24:07 +0100

    Add 1-11.scm

--- SICP/chapter1/1-3.scm
+++ SICP/chapter1/1-3.scm
@@ -2,6 +2,6 @@
  (cond ((and (> x z) (> y z)) (+ (square x) (square y)))
        ((and (> x y) (> z y)) (+ (square x) (square z)))
        (else (+ (square y) (square z)))))
-	   
+
 (define (square x)
   (* x x))
