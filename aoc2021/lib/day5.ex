cde2a9ce21e1b50d6f13af34cea9c5ff7e70d396
theAnnie <cypelll95@gmail.com>
2021-12-07 00:25:56 +0100

    Add day 5 part 2

--- aoc2021/lib/day5.ex
+++ aoc2021/lib/day5.ex
@@ -15,4 +15,19 @@ defmodule Day5 do
     |> Enum.filter(fn {_point, frequencie} -> frequencie > 1 end)
     |> Enum.count()
   end
+
+  def calc2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Stream.map(fn x -> String.replace(x, ~r/(->|,)/, " ") |> String.split() end)
+    |> Stream.map(fn x -> Enum.map(x, &String.to_integer/1) end)
+    |> Stream.flat_map(fn
+      [x, y1, x, y2] -> for(y <- y1..y2, do: {x, y})
+      [x1, y, x2, y] -> for(x <- x1..x2, do: {x, y})
+      [x1, y1, x2, y2] -> [x1..x2, y1..y2] |> Enum.map(&Enum.to_list/1) |> Enum.zip()
+    end)
+    |> Enum.frequencies()
+    |> Enum.filter(fn {_point, frequencie} -> frequencie > 1 end)
+    |> Enum.count()
+  end
 end
--- aoc2021/test/day5_test.exs
+++ aoc2021/test/day5_test.exs
@@ -23,4 +23,27 @@ defmodule Day5Test do
 
     assert result == 5
   end
+
+  test "2" do
+    # given
+    x = """
+    0,9 -> 5,9
+    8,0 -> 0,8
+    9,4 -> 3,4
+    2,2 -> 2,1
+    7,0 -> 7,4
+    6,4 -> 2,0
+    0,9 -> 2,9
+    3,4 -> 1,4
+    0,0 -> 8,8
+    5,5 -> 8,2
+    """
+
+    # when
+    result = Day5.calc2(x)
+
+    # then
+
+    assert result == 12
+  end
 end
