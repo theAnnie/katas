0c94b68073efae72b73b17e46c860283f4c02631
theAnnie <cypelll95@gmail.com>
2021-12-08 00:11:56 +0100

    Add day 7 part 2

--- aoc2021/lib/day7.ex
+++ aoc2021/lib/day7.ex
@@ -11,4 +11,18 @@ defmodule Day7 do
     |> Enum.map(fn x -> if x < median, do: median - x, else: x - median end)
     |> Enum.sum()
   end
+
+  def calc2(input) do
+    input =
+      input
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    mean = Float.floor(Statistics.mean(input))
+
+    input
+    |> Enum.map(fn x -> if x < mean, do: mean - x, else: x - mean end)
+    |> Enum.map(fn n -> n * (n + 1) / 2 end)
+    |> Enum.sum()
+  end
 end
