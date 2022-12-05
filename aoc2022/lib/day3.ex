a1ea4e4ee4b0d29fb11dcaed3f65476088cc74fb
theAnnie <cypelll95@gmail.com>
2022-12-05 13:25:29 +0100

    Add day 3 part 2

--- aoc2022/lib/day3.ex
+++ aoc2022/lib/day3.ex
@@ -24,4 +24,22 @@ defmodule Day3 do
   defp to_value(c) do
     if c > 96, do: c - 96, else: c - 38
   end
+
+  def calc2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.chunk_every(3)
+    |> Enum.map(&to_map_set2/1)
+    |> Enum.map(fn {a, b,c } -> a |> MapSet.intersection(b) |> MapSet.intersection(c) end)
+    |> Enum.map(fn x -> x |> MapSet.to_list() |> hd() |> :binary.first() end)
+    |> Enum.map(&to_value/1)
+    |> Enum.sum()
+  end
+
+  defp to_map_set2([a, b, c]) do
+    a = a |> String.graphemes() |> MapSet.new()
+    b = b |> String.graphemes() |> MapSet.new()
+    c = c |> String.graphemes() |> MapSet.new()
+    {a, b, c}
+  end
 end
--- aoc2022/test/day3_test.exs
+++ aoc2022/test/day3_test.exs
@@ -23,4 +23,27 @@ defmodule Day3Test do
 
     assert result == 7568
   end
+
+  test "part 2" do
+    given = """
+    vJrwpWtwJgWrhcsFMMfFFhFp
+    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+    PmmdzqPrVvPwwTWBwg
+    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+    ttgJtRGJQctTZtZT
+    CrZsJsPPZsGzwwsLwLmpwMDw
+    """
+
+    result = Day3.calc2(given)
+
+    assert result == 70
+  end
+
+  test "part 2 input" do
+    given = File.read!("input/3_1.txt")
+
+    result = Day3.calc2(given)
+
+    assert result == 2780
+  end
 end
