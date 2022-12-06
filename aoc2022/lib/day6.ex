3eb6f800121470ba4d56f8d23e0e2028b2625e60
theAnnie <cypelll95@gmail.com>
2022-12-06 11:48:49 +0100

    Add day 6 part 2

--- aoc2022/lib/day6.ex
+++ aoc2022/lib/day6.ex
@@ -12,4 +12,18 @@ defmodule Day6 do
       end
     end)
   end
+
+  def calc2(input) do
+    input = String.graphemes(input)
+
+    Enum.reduce_while(Enum.drop(input, 14), {14, Enum.take(input, 14)}, fn x, {counter, list} ->
+      if Enum.uniq(list) == list do
+        {:halt, counter}
+      else
+        list = list |> tl() |> Enum.reverse()
+        list = [x | list] |> Enum.reverse()
+        {:cont, {counter + 1, list}}
+      end
+    end)
+  end
 end
--- aoc2022/test/day6_test.exs
+++ aoc2022/test/day6_test.exs
@@ -16,4 +16,20 @@ defmodule Day6Test do
 
     assert result == 1896
   end
+
+  test "part 2" do
+    given = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+
+    result = Day6.calc2(given)
+
+    assert result == 19
+  end
+
+  test "part 2 input" do
+    given = File.read!("input/6.txt")
+
+    result = Day6.calc2(given)
+
+    assert result == 3452
+  end
 end
